e64d931b97bf802d1a6919da0b01a496f1d25272
Justin Fong <justinf715@gmail.com>
2015-04-01 07:12:12 -0700

    Corrected "pizza (imperative + functional)"

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -32,7 +32,7 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(productsICanEat.length).toBe(FILL_ME_IN);
+    expect(productsICanEat.length).toBe(1);
   });
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
@@ -40,8 +40,21 @@ describe("About Applying What We Have Learnt", function() {
       var productsICanEat = [];
 
       /* solve using filter() & all() / any() */
+      var hasMushroom = function(ingredient) {
+    return (ingredient === "mushrooms");
+    }
+    var noMushroom = function(x) {
+        if (!(x.ingredients.some(hasMushroom))) {
+              return true;
+        }
+    }
+    var noNuts = function (product) {
+      return !(product.containsNuts);
+    }
+    
+    productsICanEat.push(products.filter(noNuts).filter(noMushroom))
 
-      expect(productsICanEat.length).toBe(FILL_ME_IN);
+      expect(productsICanEat.length).toBe(1);
   });
 
   /*********************************************************************************/
